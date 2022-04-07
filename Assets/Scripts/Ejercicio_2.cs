2a7d2fcc02d670659a86250f95c3966b01b35df3
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-04-07 13:47:49 -0300

    Exercise 2

--- Assets/Scripts/Ejercicio_2.cs
+++ Assets/Scripts/Ejercicio_2.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Ejercicio_2 : MonoBehaviour
 {
+    public int Precio1;
+    public int Precio2;
+    public int Precio3;
+    public int DineroDisponible;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        int precioTotal = Precio1 + Precio2 + Precio3;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (precioTotal > DineroDisponible)
+        {
+            Debug.Log("La suma supera el monto de dinero disponible, faltan $" + (precioTotal - DineroDisponible));
+        }
+
+        else
+        {
+            Debug.Log("La suma no supera el monto de dinero disponible, sobrarán $" +(DineroDisponible - precioTotal));
+        }
     }
 }
