7412f493a5e17517c0157be358ba3b34f2810cc8
Vincenzo-Palonsky <47026639@est.ort.edu.ar>
2022-04-07 13:37:00 -0300

    Exercise 1

---
+++ Assets/Scripts/Ejercicio_1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio_1 : MonoBehaviour
+{
+    public int Numero1;
+    public int Numero2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Numero1 == Numero2)
+        {
+            Debug.Log("Los números son iguales.");
+        }
+
+        else
+        {
+            Debug.Log("Los números no son iguales.");
+        }
+    }
+}
